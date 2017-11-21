a6f0bcb492d0dd9c30ed9f92f7ef642c42b9377d
Paul Sinnett <paul.sinnett@gmail.com>
2017-11-21 12:00:22 +0000

    upgrade to 2017

--- Assets/Serve.cs
+++ Assets/Serve.cs
@@ -5,12 +5,13 @@ public class Serve : MonoBehaviour {
 	public Transform ball;
 	public Transform target;
 	public float force = 0.5f;
+    public float offset = 50.0f;
 
 	void Service() {
 
 		Transform newBall = Instantiate (ball);
 		Vector3 ballPosition = transform.position;
-		Vector3 targetPosition = target.position + Random.onUnitSphere * 50.0f;
+		Vector3 targetPosition = target.position + Random.onUnitSphere * offset;
 		targetPosition.y = target.position.y;
 		newBall.position = Vector3.MoveTowards (ballPosition, targetPosition, 5.0f);
 		Rigidbody ballPhysics = newBall.GetComponent<Rigidbody> ();
